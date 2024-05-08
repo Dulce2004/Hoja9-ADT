f6a975e07745bbcb935adbf84445cd4b4d10169c
Poposit <131786187+Dcextrim@users.noreply.github.com>
2024-05-08 17:42:31 -0600

    Documentacion de la clase leer_archivo_rutas

--- Hoja9-ADT/Main.py
+++ Hoja9-ADT/Main.py
@@ -4,16 +4,20 @@
 #Daniel Chet - 231177
 
 
-import networkx as nx 
-import heapq 
+import networkx as nx  # Importa la librería NetworkX para trabajar con grafos
+import heapq  # Importa el módulo heapq para trabajar con colas de prioridad
 
+
+#Lee un archivo de rutas y construye un grafo.
 def leer_archivo_rutas(archivo):
-    grafo = nx.Graph()   
+    grafo = nx.Graph()  # Crea un nuevo grafo 
 
-    with open(archivo, 'r') as file:  
-        for linea in file:  
-            origen, destino, costo = [s.strip().strip('"') for s in linea.split(',')]
-            costo = int(costo)  
-            grafo.add_edge(origen, destino, weight=costo)              
-            grafo.add_edge(destino, origen, weight=costo)  
-    return grafo
+    with open(archivo, 'r') as file:  # Abre el archivo en modo lectura
+        for linea in file:  # Itera sobre cada línea del archivo
+            origen, destino, costo = [s.strip().strip('"') for s in linea.split(',')]# Eliminar comillas y dividir por comas
+            costo = int(costo)  # Convierte el costo a entero
+
+            grafo.add_edge(origen, destino, weight=costo)  # Añade una ruta de origen a destino con el costo
+            grafo.add_edge(destino, origen, weight=costo)  # Añade una ruta de destino a origen con el mismo costo
+
+    return grafo  # Devuelve el grafo construido
