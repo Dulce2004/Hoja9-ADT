75beeb5ff81930bd920968042a3ac8a351c92b39
Poposit <131786187+Dcextrim@users.noreply.github.com>
2024-05-09 10:23:09 -0600

    Correccion de Bugs

--- Hoja9-ADT/Main.py
+++ Hoja9-ADT/Main.py
@@ -3,10 +3,8 @@
 #Dulce Ambrosio - 231143
 #Daniel Chet - 231177
 
-
 import networkx as nx  # Importa la librería NetworkX para trabajar con grafos
 import heapq  # Importa el módulo heapq para trabajar con colas de prioridad
-
 
 #Lee un archivo de rutas y construye un grafo.
 def leer_archivo_rutas(archivo):
@@ -14,20 +12,19 @@ def leer_archivo_rutas(archivo):
 
     with open(archivo, 'r') as file:  # Abre el archivo en modo lectura
         for linea in file:  # Itera sobre cada línea del archivo
-            origen, destino, costo = [s.strip().strip('"') for s in linea.split(',')]# Eliminar comillas y dividir por comas
+            # Eliminar comillas y dividir por comas
+            origen, destino, costo = [s.strip().strip('"') for s in linea.split(',')]
             costo = int(costo)  # Convierte el costo a entero
-
             grafo.add_edge(origen, destino, weight=costo)  # Añade una ruta de origen a destino con el costo
             grafo.add_edge(destino, origen, weight=costo)  # Añade una ruta de destino a origen con el mismo costo
 
-    return grafo  # Devuelve el grafo construido
+    return grafo  # Devuelve el grafo construido
 
-#Implementa el algoritmo de Dijkstra para encontrar las distancias más cortas
 def dijkstra(grafo, origen):
     
     distancias = {nodo: float('inf') for nodo in grafo.nodes()}  # Inicializa las distancias a infinito para todos los nodos
     distancias[origen] = 0  # La distancia desde el origen a sí mismo es cero
-    cola = [(0, origen)]  # Crea una cola de prioridad con la tupla (costo_actual, nodo)
+    cola = [(0, origen)]  # Crea una cola de prioridad con la (costo_actual, nodo)
 
     while cola:  # Mientras la cola no esté vacía
         costo_actual, nodo_actual = heapq.heappop(cola)  # Obtén el nodo con el menor costo actual
@@ -43,11 +40,14 @@ def dijkstra(grafo, origen):
                 distancias[vecino] = nuevo_costo  # Actualiza la distancia más corta al vecino
                 heapq.heappush(cola, (nuevo_costo, vecino))  # Agrega el vecino a la cola de prioridad
 
-    return distancias  # Devuelve las distancias más cortas
+    return distancias  # Devuelve las distancias más cortas
+
 
 # Función principal que solicita al usuario la estación de salida, calcula las mejores rutas desde esa estación
 #utilizando el algoritmo de Dijkstra, y muestra los resultados.
+
 def main():
+ 
     archivo_rutas = 'Rutas.txt'  # Nombre del archivo de rutas
     grafo = leer_archivo_rutas(archivo_rutas)  # Lee el archivo y construye el grafo
 
@@ -67,5 +67,4 @@ def main():
         if destino != estacion_salida and costo != float('inf'):  # Verifica que no sea la estación de salida y que el costo no sea infinito
             print(f"Hacia {destino}: Costo = {costo}")  # Muestra la mejor ruta y su costo
 
-if __name__ == "_main_":  # Verifica si el script se está ejecutando directamente
-    main()  # Llama a la función principal
+main()  # Llama a la función principal
